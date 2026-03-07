c271f2cd9fbd0103324a362faed68919ff2ba612
Navith-25 <wwnavith@gmail.com>
2026-03-07 11:16:10 +0530

    JWT

--- src/main/java/com/lankatransit/backend/controller/UserController.java
+++ src/main/java/com/lankatransit/backend/controller/UserController.java
@@ -2,11 +2,15 @@ package com.lankatransit.backend.controller;
 
 import com.lankatransit.backend.entity.User;
 import com.lankatransit.backend.repository.UserRepository;
+import com.lankatransit.backend.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,16 +22,23 @@ public class UserController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
     @GetMapping
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-
     @PostMapping("/register")
     public User registerUser(@RequestBody User newUser) {
         String encryptedPassword = passwordEncoder.encode(newUser.getPasswordHash());
         newUser.setPasswordHash(encryptedPassword);
+
+        if (newUser.getRole() == null || newUser.getRole().isEmpty()) {
+            newUser.setRole("PASSENGER");
+        }
 
         return userRepository.save(newUser);
     }
@@ -37,7 +48,17 @@ public class UserController {
         User user = userRepository.findByEmail(loginRequest.getEmail());
 
         if (user != null && passwordEncoder.matches(loginRequest.getPassword(), user.getPasswordHash())) {
-            return ResponseEntity.ok(user);
+
+
+            String token = jwtUtil.generateToken(user.getEmail(), user.getRole());
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("token", token);
+            response.put("role", user.getRole());
+            response.put("email", user.getEmail());
+            response.put("name", user.getName());
+
+            return ResponseEntity.ok(response);
         } else {
             return ResponseEntity.status(org.springframework.http.HttpStatus.UNAUTHORIZED)
                     .body("Invalid email or password");
